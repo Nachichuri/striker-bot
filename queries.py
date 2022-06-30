1d94c28e0a74b504ef69938aea3c5e5de6d7cda3
Nachichuri <nachichuri@gmail.com>
2022-06-30 14:23:20 -0300

    refactor: extracted query strings to a helper file

--- queries.py
+++ queries.py
@@ -1,1 +1,7 @@
-get_status_from_user = 'SELECT * FROM strikes WHERE name = ?'
+create_new_user = "INSERT INTO strikes VALUES (?, ?, ?)"
+create_strikes_table = """CREATE TABLE IF NOT EXISTS strikes (
+                            name text PRIMARY KEY,
+                            strikes integer NOT NULL,
+                            pastries integer NOT NULL
+                        )"""
+get_status_from_user = "SELECT * FROM strikes WHERE name = ?"
